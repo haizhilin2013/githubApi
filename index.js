b9aca1a70d90ea92a92d4d67e7d52ee2cead97d3
wangjian <1517@mapgoo.net>
2019-12-04 09:30:44 +0800

    555 test...

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const simpleGit = require('simple-git/promise')();
 
+// 先拉取
 simpleGit.pull('origin', 'master', {'--no-rebase': null});
 
-simpleGit.status()
-.then((status)=>{
-	console.log(status);
-	if(status.files.length > 0){
-		simpleGit.add('.');
-		simpleGit.commit('555 test...');
-		simpleGit.push('origin', 'master', ()=>{
-			console.log('doen')
-		});
-	}
-});
+// 查看状态
+simpleGit
+	.status()
+	.then((status)=>{
+		console.log(status);
+		if(status.files.length > 0){
+			simpleGit.add('.');
+			simpleGit.commit('555 test...');
+			simpleGit.push('origin', 'master', ()=>{
+				console.log('doen')
+			});
+		}
+	});
