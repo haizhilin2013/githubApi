821a18a129d64ce6f1077e76036c8bab7bf033e6
wangjian <1517@mapgoo.net>
2019-12-04 09:32:49 +0800

    test...

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ simpleGit
 		console.log(status);
 		if(status.files.length > 0){
 			simpleGit.add('.');
-			simpleGit.commit('555 test...');
+			simpleGit.commit('test...');
 			simpleGit.push('origin', 'master', ()=>{
 				console.log('done')
 			});
