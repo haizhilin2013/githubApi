8e5cdae523cd63ed9de4050e4c14d6c08d869b55
wangjian <1517@mapgoo.net>
2019-12-04 09:21:25 +0800

    555 test...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # githubApi
 测试github的API
+
+## 我来测试下
---
+++ index.js
@@ -0,0 +1,12 @@
+const simpleGit = require('simple-git/promise')();
+
+simpleGit.pull('origin', 'master', {'--no-rebase': null});
+
+simpleGit.status()
+.then((status)=>{
+	console.log(status);
+	if(status.files.length > 0){
+		simpleGit.add('.');
+		simpleGit.commit('555 test...');
+	}
+});
